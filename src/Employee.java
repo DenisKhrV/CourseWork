810f418019e6657712708ac5d3ca628a1622d9c2
DenisKhrV <DenisKhr@yandex.ru>
2023-07-13 16:52:24 +1000

    Сделаны задачи базовой сложности

---
+++ src/Employee.java
@@ -0,0 +1,93 @@
+public class Employee {
+    private final String name;
+    private int salary;
+    private String department;
+    int id;
+
+    static  int counter = 0;
+
+    public Employee(String name, int salary, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+        this.id = counter;
+        counter++;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String toString() {
+        return "ФИО: " + this.name + ". Зарплата:" + this.salary + ". Отдел: " + this.department;
+    }
+
+    public static void getListAll() {
+        for (Employee employee : Main.storage) {
+            System.out.println(employee);
+        }
+    }
+    public static int sumSalary() {
+        int sum = 0;
+        for (Employee employee : Main.storage) {
+            sum += employee.salary;
+        }
+        System.out.println("\nСумма затрат на зарплаты в месяц: "+ sum);
+        return sum;
+    }
+
+    public static void findMinSailary() {
+        int min = Main.storage[0].salary;
+        String name = Main.storage[0].name;
+        for (Employee i : Main.storage) {
+            if (i.salary < min) {
+                min = i.salary;
+                name = i.name;
+            }
+        }
+        System.out.println("\nСотрудник с минимальной зарплатой: " + name+ ". Зарплата: "+ min);
+
+    }
+    public static void findMaxSailary() {
+        int max = Main.storage[0].salary;
+        String name = Main.storage[0].name;
+        for (Employee i : Main.storage) {
+            if (i.salary > max) {
+                max = i.salary;
+                name = i.name;
+            }
+        }
+        System.out.println("\nСотрудник с максимальной зарплатой: " + name+ ". Зарплата: "+ max);
+
+    }
+    public static void findAverageSalary() {
+        int averageSalary = sumSalary() / Main.storage.length;
+        System.out.println("\nСреднее значение зарплат сотрудников: " + averageSalary +"\n");
+    }
+
+    public static void getAllName() {
+        for (Employee i : Main.storage){
+            System.out.println(i.name);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+    static  Employee[] storage = new Employee[10];
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        storage[0] = new Employee("Иванов Иван Иванович", 90000, "1");
+        storage[1] = new Employee("Иванов Иван Степанович", 105000, "2");
+        storage[2] = new Employee("Ким Дмитрий Дмитриевич", 125000, "3");
+        storage[3] = new Employee("Петров Степан Семёнович", 70000, "4");
+        storage[4] = new Employee("Медведев Александр Викторович", 120000, "2");
+        storage[5] = new Employee("Окунев Виктор Петрович", 50000, "2");
+        storage[6] = new Employee("Трутнев Сергей Петрович", 70000, "5");
+        storage[7] = new Employee("Лебедев Сергей Юрьевич", 60000, "5");
+        storage[8] = new Employee("Пушкин Артём Фёдорович", 45000, "1");
+        storage[9] = new Employee("Трутнев Сергей Петрович", 65000, "4");
+
+        Employee.getListAll();
+        Employee.sumSalary();
+        Employee.findMinSailary();
+        Employee.findMaxSailary();
+        Employee.findAverageSalary();
+        Employee.getAllName();
+
+
     }
 }
